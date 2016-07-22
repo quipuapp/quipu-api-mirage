23fec066299d6607fa0db14f9537749c29254292
Pau Compte <pausandalio@gmail.com>
2016-07-22 11:37:54 +0200

    Add relationships to income-invoice model

--- models/income-invoice.js
+++ models/income-invoice.js
@@ -1,5 +1,9 @@
-import { Model, belongsTo } from 'ember-cli-mirage';
+import { Model, belongsTo, hasMany } from 'ember-cli-mirage';
 
 export default Model.extend({
-  contact: belongsTo()
+  contact: belongsTo(),
+  items:                 hasMany('bookEntryItem'),
+  numeration:            belongsTo('numberingSeries'),
+  accountingCategory:    belongsTo(),
+  accountingSubcategory: belongsTo()
 });
